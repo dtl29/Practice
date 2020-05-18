4362b63eed50215f84b62749775c02eb934a43bf
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-05-18 15:30:48 -0400

    Add files via upload

--- DansLib/DanLib.cpp
+++ DansLib/DanLib.cpp
@@ -1,4 +1,10 @@
 
+#include "DanLib.hpp"
+#include <iostream>
+
+/*
+
+*/
 int factorial(int num)
 {
 	if(num > 1)
@@ -6,4 +12,128 @@ int factorial(int num)
 		return num * factorial(num-1);
 	}
 	return 1;
+}
+
+/*
+*****************************************************************
+*********  DansList member fucntions  *************************
+*****************************************************************
+*/
+
+//**************************************************
+// Destructor                                      *
+// This function deletes every node in the list.   *
+// pre: n/a                                        *
+// post: destroyed object                          *
+//**************************************************
+DansList::~DansList()
+{
+	node *curentnode = head;
+	node *nextnode;
+	for(int i = 0; i > size; i++)
+	{
+		nextnode = curentnode->nextnode;
+		delete curentnode;
+		curentnode = nextnode;
+	}
+}
+
+DansList::DansList()
+{
+	size = 0;
+	maxSize = 500;
+}
+
+bool DansList::empty()
+{
+	if(size == 0)
+	{
+		return true;
+	}
+	return false;
+}
+int DansList::front()
+{
+	return head->value;
+}
+int DansList::back()
+{
+	return last->value;
+}
+void DansList::pushBack(int value)
+{
+	if(size == 0)
+	{
+		head->value = value;
+		last = head;
+		size = 1;
+		return;
+	}
+	else
+	{
+		node *newNode = new node;
+		newNode->value = value;
+		last->nextnode = newNode;
+		last = newNode;
+		size += 1;
+	}
+}
+
+int DansList::sizeofList()
+{
+	return size;
+}
+
+void DansList::printList()
+{
+	std::cout << "The list: \n";
+	node *thisNode = head;
+	for(int i = 0; i < size; i++)
+	{
+		std::cout << thisNode->value << ", ";
+		thisNode = thisNode->nextnode;
+	}
+	std::cout << std::endl;
+}
+
+int DansList::popBack()
+{
+	int tempInt;
+	node *nexttoLast = head;
+	if(empty())
+	{
+		return -1;//should throw an error 
+	}
+	else
+	{
+		for(int i = 0; i < size - 1; i++)
+		{
+			nexttoLast = nexttoLast->nextnode;
+		}
+		size--;
+		tempInt = last->value;
+		delete last; 
+		last = nexttoLast;
+		return tempInt;
+	}
+}
+
+/*
+*****************************************************************
+*********  DansVector member fucntions  *************************
+*****************************************************************
+*/
+DansVector::DansVector()
+{
+	DansVector::size = 1;
+
+}
+DansVector::DansVector(int n)
+{
+	DansVector::size = n;
+}
+
+void DansVector::PushBack()
+{
+
 }
--- DansLib/main.cpp
+++ DansLib/main.cpp
@@ -4,4 +4,15 @@
 int main()
 {
 	std::cout << "Factoral of 5 = " << factorial(5) << '\n';
+	DansVector vec; 
+	DansList lis;
+	std::cout << "list should be empty (0 for false 1 for true) : " << lis.empty() << std::endl;
+	lis.pushBack(10);
+	std::cout << "lis has a front of " << lis.front() << std::endl;
+	lis.pushBack(5);
+	lis.pushBack(6);
+	std::cout << "lis has a back of " << lis.back() << '\n';
+	lis.printList();
+	std::cout << lis.popBack() << " is the poped back of the list" << std::endl;
+	lis.printList();
 }
